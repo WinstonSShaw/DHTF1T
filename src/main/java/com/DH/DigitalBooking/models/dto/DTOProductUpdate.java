c9852d774868090049ccc2cbb72eb7ead101ab53
WinstonShaw <winstonsshaw@hotmail.com>
2022-12-08 18:29:20 -0300

    primer push

---
+++ src/main/java/com/DH/DigitalBooking/models/dto/DTOProductUpdate.java
@@ -0,0 +1,30 @@
+package com.DH.DigitalBooking.models.dto;
+
+import com.DH.DigitalBooking.models.Feature;
+import com.DH.DigitalBooking.models.Image;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class DTOProductUpdate {
+
+    private Long id;
+    private String title;
+    private String description_title;
+    private String description;
+    private Integer rating;
+    private String address;
+    private long category_id;
+    private long city_id;
+    private Set<Image> images = new HashSet<>();
+    private Set<Feature> features = new HashSet<>();
+    private String policy;
+}
